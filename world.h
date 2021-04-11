a069c3f5a248401451cf730c1b2416ff6be6510c
Steven Shan <steven.shan99@gmail.com>
2021-04-11 19:18:40 -0400

    added api for deleting and setting blocks

--- world.h
+++ world.h
@@ -6,8 +6,11 @@
 // world is WORLD_SIZE x WORLD_SIZE x WORLD_SIZE
 #define WORLD_SIZE 16
 
+#define VERTICES_PER_BLOCK (6 * 3 * 2) // 6 faces, 2 triangles per face
 #define BLOCKS (WORLD_SIZE * WORLD_SIZE * WORLD_SIZE)
-#define VERTICES (WORLD_SIZE * WORLD_SIZE * WORLD_SIZE * 6 * 3 * 2)
+#define VERTICES (WORLD_SIZE * WORLD_SIZE * WORLD_SIZE * VERTICES_PER_BLOCK)
+
+#define ZYX_MAJOR(x, y, z) ((z) * WORLD_SIZE * WORLD_SIZE + (y) * WORLD_SIZE + (x))
 
 enum class Axis { X, Y, Z };
 
@@ -20,6 +23,10 @@ enum class Block : uint8_t {
 
 // Stores data for vertices that is passed into shader
 struct WorldGeometry {
+  private:
+    // maps logical block coordinate to index into list of vertices
+    int block_coordinates[WORLD_SIZE][WORLD_SIZE][WORLD_SIZE] = {};
+
   public:
     uint8_t world_buffer_data[BLOCKS] = {};
 
@@ -32,11 +39,17 @@ struct WorldGeometry {
     // the texture position of each vertex
     uint8_t vertex_texture_uv_data[VERTICES];
 
-    unsigned int num_blocks = 0;
     unsigned int num_vertices = 0;
 
     WorldGeometry() {
-        // Init world
+        for (int x = 0; x < WORLD_SIZE; ++x) {
+            for (int y = 0; y < WORLD_SIZE; ++y) {
+                for (int z = 0; z < WORLD_SIZE; ++z) {
+                    block_coordinates[x][y][z] = -1;
+                }
+            }
+        }
+
         for (int x = 0; x < WORLD_SIZE; ++x) {
             for (int y = 0; y < WORLD_SIZE; ++y) {
                 for (int z = 0; z < WORLD_SIZE; ++z) {
@@ -53,14 +66,7 @@ struct WorldGeometry {
                     }
 
                     if (face_type != Block::Air) {
-                        add_square(face_type, x, y, z, Axis::X);
-                        add_square(face_type, x, y, z, Axis::Y);
-                        add_square(face_type, x, y, z, Axis::Z);
-                        add_square(face_type, x + 1, y, z, Axis::X);
-                        add_square(face_type, x, y + 1, z, Axis::Y);
-                        add_square(face_type, x, y, z + 1, Axis::Z);
-
-                        world_buffer_data[z * WORLD_SIZE * WORLD_SIZE + y * WORLD_SIZE + x] = (uint8_t)face_type;
+                        set_block(x, y, z, face_type);
                     }
                 }
             }
@@ -68,10 +74,49 @@ struct WorldGeometry {
     }
 
   private:
-    void add_square(Block block_face_type, int x, int y, int z, Axis norm) {
-        const unsigned int vertex = num_vertices;
-        num_vertices += 6;
+    void set_block(int x, int y, int z, Block block) {
+        if (block == Block::Air) {
+            delete_block(x, y, z);
+            return;
+        }
 
+        int vertex = block_coordinates[x][y][z];
+        bool new_block = false;
+        if (vertex == -1) {
+            new_block = true;
+            vertex = num_vertices;
+        }
+
+        add_square(block, vertex, x, y, z, Axis::X);
+        add_square(block, vertex, x, y, z, Axis::Y);
+        add_square(block, vertex, x, y, z, Axis::Z);
+        add_square(block, vertex, x + 1, y, z, Axis::X);
+        add_square(block, vertex, x, y + 1, z, Axis::Y);
+        add_square(block, vertex, x, y, z + 1, Axis::Z);
+
+        if (new_block) {
+            num_vertices = vertex;
+        }
+
+        world_buffer_data[ZYX_MAJOR(x, y, z)] = (uint8_t)block;
+    }
+
+    void delete_block(int x, int y, int z) {
+        int vertex = block_coordinates[x][y][z];
+        if (vertex != -1) {
+            num_vertices -= VERTICES_PER_BLOCK;
+
+            world_buffer_data[ZYX_MAJOR(x, y, z)] =  (uint8_t)Block::Air;
+
+            int remainder = num_vertices - vertex - VERTICES_PER_BLOCK;
+
+            memmove(&block_face_data[vertex], &block_face_data[vertex + VERTICES_PER_BLOCK], remainder);
+            memmove(&vertex_data[vertex], &vertex_data[vertex + VERTICES_PER_BLOCK], remainder);
+            memmove(&vertex_texture_uv_data[vertex], &vertex_texture_uv_data[vertex + VERTICES_PER_BLOCK], remainder);
+        }
+    }
+
+    void add_square(Block block_face_type, int &vertex, int x, int y, int z, Axis norm) {
         glm::vec3 p0, p1, p2, p3;
         p0 = glm::vec3(x, y, z);
         uint8_t o = 1;
@@ -117,6 +162,8 @@ struct WorldGeometry {
         for (unsigned int i = 0; i < 6; i++) {
             block_face_data[vertex + i] = (uint8_t)block_face_type;
         }
+
+        vertex += 6;
     }
 };
 
