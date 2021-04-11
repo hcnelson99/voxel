c9584ec1a55ac2b2fee7378dc8897fa3c687462c
Steven Shan <steven.shan99@gmail.com>
2021-04-11 19:24:54 -0400

    separated world api from implementation

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
-voxel: voxel.cpp world.h
-	g++ -o voxel voxel.cpp -g -std=c++11 -pthread -lSDL2 -lGLEW -lX11 -lGLU -lGL -DGLM_ENABLE_EXPERIMENTAL
+CXXFLAGS=-g -std=c++11 -pthread -lSDL2 -lGLEW -lX11 -lGLU -lGL -DGLM_ENABLE_EXPERIMENTAL
+
+voxel: voxel.cpp world.cpp world.h
+	g++ -o voxel world.cpp voxel.cpp $(CXXFLAGS)
--- world.h
+++ world.h
@@ -50,120 +50,13 @@ struct WorldGeometry {
             }
         }
 
-        for (int x = 0; x < WORLD_SIZE; ++x) {
-            for (int y = 0; y < WORLD_SIZE; ++y) {
-                for (int z = 0; z < WORLD_SIZE; ++z) {
-                    int r = rand() % 10;
-                    Block face_type;
-                    if (r == 0) {
-                        face_type = Block::Stone;
-                    } else if (r == 1) {
-                        face_type = Block::Dirt;
-                    } else if (r == 2) {
-                        face_type = Block::Wood;
-                    } else {
-                        face_type = Block::Air;
-                    }
-
-                    if (face_type != Block::Air) {
-                        set_block(x, y, z, face_type);
-                    }
-                }
-            }
-        }
+        randomly_initialize();
     }
 
   private:
-    void set_block(int x, int y, int z, Block block) {
-        if (block == Block::Air) {
-            delete_block(x, y, z);
-            return;
-        }
-
-        int vertex = block_coordinates[x][y][z];
-        bool new_block = false;
-        if (vertex == -1) {
-            new_block = true;
-            vertex = num_vertices;
-        }
-
-        add_square(block, vertex, x, y, z, Axis::X);
-        add_square(block, vertex, x, y, z, Axis::Y);
-        add_square(block, vertex, x, y, z, Axis::Z);
-        add_square(block, vertex, x + 1, y, z, Axis::X);
-        add_square(block, vertex, x, y + 1, z, Axis::Y);
-        add_square(block, vertex, x, y, z + 1, Axis::Z);
-
-        if (new_block) {
-            num_vertices = vertex;
-        }
-
-        world_buffer_data[ZYX_MAJOR(x, y, z)] = (uint8_t)block;
-    }
-
-    void delete_block(int x, int y, int z) {
-        int vertex = block_coordinates[x][y][z];
-        if (vertex != -1) {
-            num_vertices -= VERTICES_PER_BLOCK;
-
-            world_buffer_data[ZYX_MAJOR(x, y, z)] =  (uint8_t)Block::Air;
-
-            int remainder = num_vertices - vertex - VERTICES_PER_BLOCK;
-
-            memmove(&block_face_data[vertex], &block_face_data[vertex + VERTICES_PER_BLOCK], remainder);
-            memmove(&vertex_data[vertex], &vertex_data[vertex + VERTICES_PER_BLOCK], remainder);
-            memmove(&vertex_texture_uv_data[vertex], &vertex_texture_uv_data[vertex + VERTICES_PER_BLOCK], remainder);
-        }
-    }
-
-    void add_square(Block block_face_type, int &vertex, int x, int y, int z, Axis norm) {
-        glm::vec3 p0, p1, p2, p3;
-        p0 = glm::vec3(x, y, z);
-        uint8_t o = 1;
-        if (norm == Axis::Z) {
-            p1 = glm::vec3(x + 1, y, z);
-            p2 = glm::vec3(x, y + 1, z);
-            p3 = glm::vec3(x + 1, y + 1, z);
-        } else if (norm == Axis::X) {
-            p1 = glm::vec3(x, y + 1, z);
-            p2 = glm::vec3(x, y, z + 1);
-            p3 = glm::vec3(x, y + 1, z + 1);
-            o = 0;
-        } else if (norm == Axis::Y) {
-            p1 = glm::vec3(x + 1, y, z);
-            p2 = glm::vec3(x, y, z + 1);
-            p3 = glm::vec3(x + 1, y, z + 1);
-        }
-
-        {
-            unsigned int id = 0;
-
-            vertex_data[vertex + id++] = p0;
-            vertex_data[vertex + id++] = p1;
-            vertex_data[vertex + id++] = p3;
-
-            vertex_data[vertex + id++] = p0;
-            vertex_data[vertex + id++] = p2;
-            vertex_data[vertex + id++] = p3;
-        }
-
-        {
-            unsigned int id = 0;
-
-            vertex_texture_uv_data[vertex + id++] = 3;
-            vertex_texture_uv_data[vertex + id++] = 2 - o;
-            vertex_texture_uv_data[vertex + id++] = 0;
-
-            vertex_texture_uv_data[vertex + id++] = 3;
-            vertex_texture_uv_data[vertex + id++] = 1 + o;
-            vertex_texture_uv_data[vertex + id++] = 0;
-        }
-
-        for (unsigned int i = 0; i < 6; i++) {
-            block_face_data[vertex + i] = (uint8_t)block_face_type;
-        }
-
-        vertex += 6;
-    }
+    void randomly_initialize();
+    void set_block(int x, int y, int z, Block block);
+    void delete_block(int x, int y, int z);
+    void add_square(Block block_face_type, int &vertex, int x, int y, int z, Axis norm);
 };
 
