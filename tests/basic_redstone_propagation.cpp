4908ef38e6d57fd6a1c1f99d1128f13f251e9a13
Steven Shan <steven.shan99@gmail.com>
2021-04-19 11:36:41 -0400

    add more redstone unit tests

--- tests/basic_redstone_propagation.cpp
+++ tests/basic_redstone_propagation.cpp
@@ -53,8 +53,59 @@ void test_or_gate() {
     assert(world.get_block(3, 0, 1).is(Block::ActiveRedstone));
 }
 
-int main() {
+void test_off_ring() {
+    world.set_block(0, 0, 0, Block::InactiveRedstone);
+    world.set_block(1, 0, 0, Block::InactiveRedstone);
+    world.set_block(2, 0, 0, Block::InactiveRedstone);
+    world.set_block(2, 1, 0, Block::InactiveRedstone);
+    world.set_block(2, 2, 0, Block::InactiveRedstone);
+    world.set_block(1, 2, 0, Block::InactiveRedstone);
+    world.set_block(0, 2, 0, Block::InactiveRedstone);
+    world.set_block(0, 1, 0, Block::InactiveRedstone);
 
+    world.tick();
+
+    assert(world.get_block(0, 0, 0).is(Block::InactiveRedstone));
+    assert(world.get_block(1, 0, 0).is(Block::InactiveRedstone));
+    assert(world.get_block(2, 0, 0).is(Block::InactiveRedstone));
+    assert(world.get_block(2, 1, 0).is(Block::InactiveRedstone));
+    assert(world.get_block(2, 2, 0).is(Block::InactiveRedstone));
+    assert(world.get_block(1, 2, 0).is(Block::InactiveRedstone));
+    assert(world.get_block(0, 2, 0).is(Block::InactiveRedstone));
+    assert(world.get_block(0, 1, 0).is(Block::InactiveRedstone));
+}
+
+void test_off_ring2() {
+    world.set_block(0, 0, 0, Block::InactiveRedstone);
+    world.set_block(1, 0, 0, Block::InactiveRedstone);
+    world.set_block(2, 0, 0, Block::InactiveRedstone);
+    world.set_block(2, 1, 0, Block::InactiveRedstone);
+    world.set_block(2, 2, 0, Block::InactiveRedstone);
+    world.set_block(1, 2, 0, Block::InactiveRedstone);
+    world.set_block(0, 2, 0, Block::InactiveRedstone);
+    world.set_block(0, 1, 0, Block::InactiveRedstone);
+    world.set_block(3, 0, 0, Block(Block::NotGate, Orientation::PosX));
+    world.set_block(4, 0, 0, Block::InactiveRedstone);
+
+    const auto check = []() {
+        assert(world.get_block(0, 0, 0).is(Block::InactiveRedstone));
+        assert(world.get_block(1, 0, 0).is(Block::InactiveRedstone));
+        assert(world.get_block(2, 0, 0).is(Block::InactiveRedstone));
+        assert(world.get_block(2, 1, 0).is(Block::InactiveRedstone));
+        assert(world.get_block(2, 2, 0).is(Block::InactiveRedstone));
+        assert(world.get_block(1, 2, 0).is(Block::InactiveRedstone));
+        assert(world.get_block(0, 2, 0).is(Block::InactiveRedstone));
+        assert(world.get_block(0, 1, 0).is(Block::InactiveRedstone));
+        assert(world.get_block(4, 0, 0).is(Block::ActiveRedstone));
+    };
+
+    world.tick();
+    check();
+    world.tick();
+    check();
+}
+
+int main() {
     test_straight_line();
 
     world.reset();
@@ -62,6 +113,12 @@ int main() {
     test_or_gate();
 
     world.reset();
+
+    test_off_ring();
+
+    world.reset();
+
+    test_off_ring2();
 
     return 0;
 }
