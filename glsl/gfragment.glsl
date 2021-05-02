de10a876a46e838332a472ede228ddef30ba37e4
Steven Shan <steven.shan99@gmail.com>
2021-05-02 18:19:10 -0400

    minor optimizations to g shaders

--- glsl/gfragment.glsl
+++ glsl/gfragment.glsl
@@ -3,31 +3,18 @@
 layout (binding = 0) uniform sampler2D terrain_texture;
 
 in vec3 world_pos;
-flat in uint block_id;
 in vec2 texture_uv;
+flat in vec3 normal;
 
 layout (location = 0) out vec3 g_position;
 layout (location = 1) out vec3 g_normal;
 layout (location = 2) out vec4 g_color_spec;
 
 void main() {
-    vec3 dFdxPos = dFdx(world_pos);
-    vec3 dFdyPos = dFdy(world_pos);
-    vec3 normal = normalize(cross(dFdxPos,dFdyPos));
-
-    float albedo = dot(normal, normalize(vec3(2, 3, 1)));
-
-    albedo = (albedo + 1) / 2;
-
-    vec2 tile_size = vec2(1, 1) / 16;
-    vec2 tile_offset = vec2(block_id % 16, block_id / 16) / 16;
-
-    vec2 tex_coord = tile_offset + tile_size * texture_uv;
-
-    vec3 color = texture(terrain_texture, tex_coord).rgb;
+    vec3 color = texture(terrain_texture, texture_uv).rgb;
 
     g_position = world_pos;
     g_normal = normal;
-    g_color_spec.rgb = color; // albedo multiplied in for now...
+    g_color_spec.rgb = color;
     g_color_spec.a = 1; // no specularity for now...
 }
--- glsl/gvertex.glsl
+++ glsl/gvertex.glsl
@@ -6,9 +6,9 @@ layout (binding = 3) uniform usampler3D block_map;
 
 layout (location = 2) uniform mat4 camera;
 
-flat out uint block_id;
 out vec3 world_pos;
 out vec2 texture_uv;
+flat out vec3 normal;
 
 // this should insert a line with const uint world_size = WORLD_SIZE
 #inject
@@ -16,6 +16,15 @@ out vec2 texture_uv;
 #include "util.glsl"
 #include "block_map.glsl"
 #include "face_orientation_to_block_id.glsl"
+
+const vec3[] normals = vec3[](
+    vec3(-1, 0, 0),
+    vec3(1, 0, 0),
+    vec3(0, -1, 0),
+    vec3(0, 1, 0),
+    vec3(0, 0, -1),
+    vec3(0, 0, 1)
+);
 
 void main() {
     uint face = (texture_uv_in >> 5) & 7;
@@ -34,9 +43,15 @@ void main() {
     vec3 vertex_coord = vpos + offset;
 
     uint block = block_at(vpos);
+    uint block_id = face_orientation_to_block_id(block, face);
 
-    block_id = face_orientation_to_block_id(block, face);
     gl_Position = camera * vec4(vertex_coord, 1);
     world_pos = vertex_coord;
-    texture_uv = vec2((texture_uv_in >> 1) & 1, texture_uv_in & 1);
+
+    vec2 tile_offset = vec2(block_id % 16, block_id / 16) / 16;
+    vec2 _texture_uv = vec2((texture_uv_in >> 1) & 1, texture_uv_in & 1) / 16;
+
+    texture_uv = tile_offset + _texture_uv;
+
+    normal = normals[face];
 }
